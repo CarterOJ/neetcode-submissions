e216a647bd3d2db3f7471433a68724e93f634cc5
Carter Johnson <145401381+CarterOJ@users.noreply.github.com>
2026-06-05 21:39:36 -0600

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if strs:
+            return "\\e".join(strs) + "\\e"
+        return ""
+
+    def decode(self, s: str) -> List[str]:
+        decoded_list = []
+        current_str = []
+        input_length = len(s)
+        i = 0
+        while i <= input_length - 1:
+            if s[i] == "\\" and s[i+1] == "e":
+                decoded_list.append("".join(current_str))
+                current_str = []
+                i += 2
+            else:
+                current_str.append(s[i])
+                i += 1
+        return decoded_list
